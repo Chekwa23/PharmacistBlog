96af45f0a50d3256ca83f604e8e951a46c97b68f
Chekwa23 <55670771+Chekwa23@users.noreply.github.com>
2020-07-25 21:31:07 -0500

    Updates

---
+++ precache-manifest.be943317491a61e249dccd04ab9a9637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08172cb9d2b99779869a4b925f212311",
+    "url": "/TestDeploy/index.html"
+  },
+  {
+    "revision": "3c6f56df46f58090a985",
+    "url": "/TestDeploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d2a5cb8fd95a5216e57",
+    "url": "/TestDeploy/static/js/2.e6d5e801.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestDeploy/static/js/2.e6d5e801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6f56df46f58090a985",
+    "url": "/TestDeploy/static/js/main.21e602bf.chunk.js"
+  },
+  {
+    "revision": "9aff33a5f81e7d4975d8",
+    "url": "/TestDeploy/static/js/runtime-main.64cff176.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestDeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
